aaea0d320c29c97049c92b7a2058a8b94d76f84c
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2020-08-03 14:13:14 -0700

    started header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,30 @@
 <template>
-    <div id="header">
-
-    </div>
+  <div id="header">
+  <div id="headerImageContainer">
+    <img src="~@/assets/logo.png" id="headerImage" />
+  </div>
+  </div>
 </template>
 <style scoped>
-
 #header {
-    position: sticky;
-    height: 80px;
-    width: 100%;
-    top: 0;
-    background-color: #3fb1ea;
-    /* opacity: 80%; */
+  position: sticky;
+  height: 80px;
+  width: 100%;
+
+  top: 0;
+  background-color: #3fb1ea;
+  /* opacity: 80%; */
+}
+#headerImageContainer {
+    float: left;
+    padding: 8px;
+}
+#headerImage {
+  height: 80%;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+  background: white;
 }
 </style>
