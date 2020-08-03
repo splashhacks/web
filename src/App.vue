208bf55b63563688a05faafae6596af4a6f09567
jacobzwang <zwang.jacob@gmail.com>
2020-08-03 11:52:12 -0700

    integrate waves with vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div style="height: 100vh">
+      <img alt="Vue logo" src="./assets/logo.png" style=" width: 50vw;" />
+    </div>
+    <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+};
 </script>
 
 <style>
