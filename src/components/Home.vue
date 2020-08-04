ec4e1bcab62dc8ac88706ecd6cd61c7ca2523a8d
Ethan Hopkins <ethan-github@die.net>
2020-08-03 18:37:30 -0700

    someUpdates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,8 +11,18 @@
       </div> 
       <div class="infoBox">
       <!-- Placeholder image (probably doesn't need an image anyway) -->
-      <div><img src="~@/assets/helping.jpg" id="medImg"></div>
-      <div><p>Welcome to Splash!</p></div>
+      <div><img src="~@/assets/helping.jpg" id="infoImg"></div>
+      <div id="infoDetails"><pre>
+        While most online Hackathons are great experiences 
+        for learning, testing out new ideas, and advancing 
+        technical skills, they tend to focus on individual 
+        teams more than the Hacker Community as a whole. 
+        At SplashHacks, we aim to provide a stimulating and 
+        deeply interconnected event for new and advanced 
+        programmers, graphic designers, and technical visionaries 
+        alike. During our event, high school and college students 
+        will have a 36-hour period to bring their ideas to life, 
+        filled with events for peer-to-peer connection and collaboration. </pre></div>
       </div>
     </div>
     <div class="content">
@@ -23,10 +33,36 @@
       </div>
     </div>
     <div class="content">
-     
-       <div class="card">content goes here</div> 
+      <div class="card">
+      <h1 class="subtitle">
+      Frequently Asked Questions
+      </h1>
+      </div>
+    </div>
+    <div class="content">
+      <div class="card">
+      <h1 class="subtitle">
+      Sponsors
+      </h1>
+      </div>
+    </div>
+    <div class="content">
+      <div class="card">
+      <h1 class="subtitle">
+      SplashHacks Team
+      </h1>
+      </div>
+    </div>
+    <div class="bottomNavBar">
+      <div class="card">
+      <h1 class="subtitle">
+      SplashHacks Team
+      </h1>
+      </div>
     </div>
   </div>
+ 
+  
 
 </template>
 <script>
@@ -39,9 +75,33 @@ export default {
 </script>
 <style scoped>
 
+.bottomNavBar {
+  background-color:#3fb1ea;
+  overflow:hidden;
+  position:relative;
+  bottom:0%;
+  width:100%;
+}
+
 .infoBox {
+  display:flex;
   flex-wrap:wrap;
-  flex-direction:column-reverse;
+  flex-direction:row;
+  justify-content:center;
+}
+
+#infoImg {
+  height:35vh;
+  vertical-align:-15%;
+  border-radius:50%;
+
+}
+
+#infoDetails {
+  text-align:left;
+  justify-content:center;
+
+
 }
 
 #smallImg {
@@ -49,11 +109,7 @@ export default {
   vertical-align:-15%;
 }
 
-#medImg {
-  height:35vh;
-  vertical-align:-15%;
-  border-radius:50%
-}
+
 
 .content {
   height: 65vh;
