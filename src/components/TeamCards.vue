4f36af890d574e89ed41a9a1a9d2122c1291ebe5
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2020-08-03 22:08:33 -0700

    building out the team card

---
+++ src/components/TeamCards.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <a :href="id" class="link">
+    {{ image }}
+  </a>
+   <img src= image id="smallImg" />
+  </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+  props: ["id", "Path"],
+};
+</script>
---
+++ src/components/team.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <TeamCards image = "~@/assets/henrymarks.jpeg"/>
+    <TeamCards image = "~@/assets/ethan.jpeg"/>
+    <TeamCards image = "~@/assets/jacob.jpeg"/>
+    <TeamCards image = "~@/assets/jessica.jpeg"/>
+    <TeamCards image = "~@/assets/jason.jpeg"/>
+    <TeamCards image = "~@/assets/swerd.jpeg"/>
+  </div>
+</template>
+<style scoped>
+</style>
+<script>
+import TeamCards from "./TeamCards.vue";
+export default {
+  components: {
+    TeamCards,
+  },
+};
+</script>
