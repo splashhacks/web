25752230d3b9192a9295952043795d56f0b8db02
JacobZwang <zwang.jacob@gmail.com>
2020-08-05 23:05:47 -0700

    clean register button

--- src/components/RegisterButton.vue
+++ src/components/RegisterButton.vue
@@ -2,35 +2,25 @@
   <a href="https://forms.gle/HpvPySwZnRnmfyGB9" class="defaultLink" :id="type">Register now</a>
 </template>
 <style scoped>
-
-
 .defaultLink {
   border: 2px solid #003c7e;
   color: #003c7e;
   text-transform: uppercase;
-  font-weight: 900;
   text-decoration: none;
   padding: 10px;
-  border-radius: 16px;
-  transition: 250ms ease; 
+  border-radius: 4px;
+  transition: 250ms ease;
+  display: inline-block;
 }
 
 .defaultLink:hover {
   background-color: #003c7e;
   color: white;
 }
-#signUp {
-  font-size: 16px;
-}
-
-#signUpBig {
-  font-size:40px;
-}
-
 </style>
 
 <script>
 export default {
-  props:["type"]
-}
+  props: ["type"],
+};
 </script>
