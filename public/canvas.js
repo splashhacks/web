7953ac838802ca6a746d86f732c52440584cf46c
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2020-08-07 09:59:48 -0700

    make splash adaptable

--- public/canvas.js
+++ public/canvas.js
@@ -1,3 +1,8 @@
-canvas = document.getElementById("canvas");
+let canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+window.onresize = function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+};
