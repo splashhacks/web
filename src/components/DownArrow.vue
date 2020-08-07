b580b44d81f2b10d4ad7efbad67d6c1cd7ae288f
jacobzwang <zwang.jacob@gmail.com>
2020-08-07 11:59:54 -0700

    animate down arrow

--- src/components/DownArrow.vue
+++ src/components/DownArrow.vue
@@ -1,17 +1,33 @@
 <template>
-       <div class="downArrow">
-        <a href="#topHeader"><img src="@/assets/anchor.svg" /></a>
-       </div>
+  <div class="downArrow">
+    <a href="#topHeader">
+      <img src="@/assets/anchor.svg" />
+    </a>
+  </div>
 </template>
 
 <style>
-.downArrow{
+.downArrow {
   position: absolute;
   left: 50%;
-  bottom: 40px;
+  bottom: 50px;
   height: 30px;
   width: 30px;
   transform: translateX(-50%) scale(2);
+  animation: MoveUpDown 1s linear infinite;
+  transition: 200ms;
+}
+:hover.downArrow {
+  transform: translateX(-50%) scale(3);
 }
 
+@keyframes MoveUpDown {
+  0%,
+  100% {
+    bottom: 50px;
+  }
+  50% {
+    bottom: 80px;
+  }
+}
 </style>
