f2061f0f24d4a8fca5fba691e4bb3f7bfbdf139f
Ethan Hopkins <ethan-github@die.net>
2020-08-07 09:09:23 -0700

    add showResponse() function

--- src/components/FAQParts.vue
+++ src/components/FAQParts.vue
@@ -1,5 +1,6 @@
 <template>
  <div>
+
      <button class="accordionSection" onclick="showResponse()">{{question}}</button>
     <div class="slidePanel">
         <p>{{response}}</p>
@@ -14,10 +15,30 @@
 }
 
 .slidePanel {
+max-height:null;
+}
 
+.slidePanelShown {
+max-height:10px;
 }
 
 </style>
+
 <script>
 
+function showResponse() {
+this.classList.toggle("active")
+var panel = this.nextElementSibling;
+if (panel.className.includes("Shown")){
+    panel.className = panel.ClassName.replace("Shown", "");
+} else {
+    panel.className += "Shown"
+}
+}
+
+</script>
+<script>
+export default {
+  props: ["question", "response"],
+};
 </script>
